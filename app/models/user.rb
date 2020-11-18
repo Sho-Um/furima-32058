2241940c1792ec7299b2ec65ad8b07a66e11a1d0
Sho-Um <zillion.of.zest.stay.gold.sho1@gmail.com>
2020-11-18 19:21:46 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence :true
-  validates :birth_date, presence :true
+  validates :nickname, presence: true
+  validates :birth_date, presence: true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :encypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
@@ -15,7 +15,7 @@ class User < ApplicationRecord
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /^([ァ-ン]|ー)+$/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A([ァ-ン]|ー)+\z/, message: '全角カタカナを使用してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
