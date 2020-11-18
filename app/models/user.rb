a445c8a3413fecede7168ad0e888c0ca95c75a9e
Sho-Um <zillion.of.zest.stay.gold.sho1@gmail.com>
2020-11-18 18:55:19 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence :true
+  validates :birth_date, presence :true
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :encypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name
+    validates :first_name
+  end
+
+  with_options presence: true, format: { with: /^([ァ-ン]|ー)+$/, message: '全角カタカナを使用してください' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
 end
