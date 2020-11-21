9400e13f0894b18801ecbb2d53b7c6cbc674e133
Sho-Um <zillion.of.zest.stay.gold.sho1@gmail.com>
2020-11-21 10:28:05 +0900

    Itemモデルへのバリデーション追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,13 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee_status_id
   belongs_to :prefecture_id
   belongs_to :scheduled_delivery_id
+
+  validates :name, :info, :user_id, presence: true
+
+  VALID_PRICEL_HALF = /\A[0-9]+\z/
+
+  validates :price, presence: true, format: {with: VALID_PRICEL_HALF}, length: {minimum: 3, maxinum: 7}, numericality: { only_integer: true, greater_than: 300, less_than: 10000000} 
+  
+  validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, numericality: {other_than: 1}
   
 end
