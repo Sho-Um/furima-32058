9aedc2d6411fba0c41bc20bd383ad1a0a07e677a
Sho-Um <zillion.of.zest.stay.gold.sho1@gmail.com>
2020-11-27 14:49:23 +0900

    UserItemOrderモデルの編集

---
+++ app/models/user_item_order.rb
@@ -0,0 +1,15 @@
+class UserItemOrder
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+
+    validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city, presence: true
+    validates :address, presence: true
+    validates :phone_number, presence: true, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
+
+    def save
+      user_item = UserItem.create(user_id: user_id, item_id: item_id)
+      Order.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, user_item_id: user_item.id)
+    end
+end
