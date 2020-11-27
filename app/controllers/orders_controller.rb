f190c8f083eda61421e6bd1703b3933e6df02138
Sho-Um <zillion.of.zest.stay.gold.sho1@gmail.com>
2020-11-27 14:48:41 +0900

    コントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+  def index
+    @user_item_order = UserItemOrder.new
+  end
+
+  def create
+    @user_item_order = UserItemOrder.new(order_params)
+    if @user_item_order.valid?
+      @user_item_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+  
+  def order_params
+    params.permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :item_id, :user_item_id).merge(user_id: current_user.id)
+  end
 end
