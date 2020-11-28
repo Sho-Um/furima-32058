9617de1eee867889c36bdaaa83ac82187c1bbdb9
Sho-Um <zillion.of.zest.stay.gold.sho1@gmail.com>
2020-11-28 15:21:06 +0900

    card.jsの修正

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,5 @@
 const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  Payjp.setPublicKey("pk_test_be9977a44ab768a3721f69c7");
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
